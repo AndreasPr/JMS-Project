72a3ce99fcfa942687c8418e3b0884140e60dfdd
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-11-11 12:13:49 -0500

    first commit

---
+++ src/test/java/andreasgroup/jms/JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package andreasgroup.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
